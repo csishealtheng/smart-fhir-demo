ba12260df2dfed37e0d2f8ffedba35b9a31d8afb
Per Johansson <per.johansson@csis.ie>
2022-10-25 10:43:49 +0100

    add launcher and redirect page

---
+++ src/components/Launcher.js
@@ -0,0 +1,25 @@
+import { oauth2 as SMART } from "fhirclient";
+import React, { useEffect } from "react";
+
+
+function Launcher() {
+
+    useEffect(() => {
+        SMART.authorize({
+            clientId: "693a2772-1ebc-4f0d-b397-a134f1861746",
+            scope: "patient/Patient.read patient/Observation.read launch/patient online_access openid profile",
+            redirectUri: "http://localhost:3000/patient",
+            iss:
+                "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d"
+
+        });
+
+    }, []);
+
+    return (
+        <div>"Launching..."</div>
+    )
+
+}
+
+export default Launcher;
---
+++ src/pages/launcher.js
@@ -0,0 +1,5 @@
+import Launcher from "../components/Launcher";
+
+export default function AppLauncher() {
+    return (<Launcher/>)
+}
