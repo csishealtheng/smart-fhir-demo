29b8c795d809098db2ac720991b3c054409567af
Per Johansson <per.johansson@csis.ie>
2022-10-26 08:02:39 +0100

    switch to EHR launch mode

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Demo app using the Cerner Code Console at https://code-console.cerner.com/console to launch a SMART App.
 
-1. Open localhost:3000/launcher
+1. For an EHR launch, open http://localhost:3000/launcher?iss=https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d
 2. Authenticate with nancysmart/Cerner01
 3. Proceed despite warnings about unknown app.
 4. Patient data should be displayed at http://localhost:3000/patient
--- src/components/Launcher.js
+++ src/components/Launcher.js
@@ -9,8 +9,9 @@ function Launcher() {
             clientId: "693a2772-1ebc-4f0d-b397-a134f1861746",
             scope: "patient/Patient.read patient/Observation.read launch/patient online_access openid profile",
             redirectUri: "http://localhost:3000/patient",
-            iss:
-                "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d"
+            
+            // iss parameter set here when doing a standalone launch
+            //iss: "https://fhir-myrecord.cerner.com/dstu2/ec2458f2-1e24-41c8-b71b-0e701af7583d"
 
         });
 
